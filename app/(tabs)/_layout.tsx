14f65029470f1362b96e743c3ea49d943ded94e1
Safarboy ibn Anvar al-Xorazmiy <safarboyalkhwarizmi@gmail.com>
2025-03-14 16:54:36 +0500

    Navbar styles compleated.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,141 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs, useNavigation, useRouter } from "expo-router";
+import React, { useEffect, useRef, useState } from "react";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  Dimensions,
+  Keyboard,
+  KeyboardAvoidingView,
+  Pressable,
+  ScrollView,
+  Text,
+  View
+} from "react-native";
+import { useRouteInfo } from "expo-router/build/hooks";
+import { StatusBar } from "react-native";
+import HomeIcon from "@/assets/images/navbar/HomeIcon.svg";
+import HomeIconActive from "@/assets/images/navbar/HomeIconActive.svg";
+import CargoAddIcon from "@/assets/images/navbar/CargoAddIcon.svg";
 
 export default function TabLayout() {
+  const navigation = useNavigation();
   const colorScheme = useColorScheme();
+  const router = useRouter();
+  const routeInfo = useRouteInfo();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
+
+      <Tabs
+        // initialRouteName="index"
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          headerShown: true,
+          tabBarStyle: {
+            height: 80, // Adjust height as needed
+            paddingTop: 10, // Ensure icons/text align properly
+            paddingBottom: 10, // Ensure icons/text align properly
+            // paddingLeft: 65,
+            // paddingRight: 39,
+            backgroundColor: "white",
+            elevation: 0, // Optional: Adds shadow effect for better UI
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+
+            borderTopColor: "#D1D1D1",
+            borderTopWidth: 1,
+          },      
+          headerStyle: {
+            borderWidth: 0,
+            elevation: 0,
+            shadowOpacity: 0
           },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          headerLeft: () => (
+            <View>
+
+            </View>
+          ),
+          headerTitle: () => (
+            <View>
+              
+              
+            </View>
+          ),
+          headerRight: () => (
+            <View
+              // style={{
+              //   paddingRight: 8,
+              //   display: "flex",
+              //   flexDirection: "row",
+              //   alignItems: "center"
+              // }}
+            ></View>
+          )
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+      >
+        {/* 🌟 LOGO (Home) */}
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarButton: () => (
+              <Pressable
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "50%", // Adjust width
+                  // backgroundColor:"red",
+                  paddingLeft: 65,
+                  height: 60, // Adjust height        
+                }}
+                onPress={() =>
+                  routeInfo.pathname === "/"
+                    ? {}
+                    : router.push("/")
+                }
+              >
+                {routeInfo.pathname === "/" ? (
+                  <View>
+                    <HomeIconActive />
+                  </View>
+                ) : (
+                  <View>
+                    <HomeIcon />
+                  </View>
+                )}
+              </Pressable>
+            )
+          }}
+        />
+
+        {/* 🌟 LOGO (CargoAdd) */}
+        <Tabs.Screen
+          name="cargoAdd"
+          options={{
+            headerShown: false,
+            title: "",
+            tabBarLabelStyle: {
+              display: "none"
+            },
+            tabBarButton: () => (
+              <Pressable
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  // backgroundColor:"red",
+                  paddingRight: 39,
+                  width: "100%",
+                  height: 60,
+                }}
+                onPress={() => router.push("/cargoAdd")}
+              >
+                <CargoAddIcon />
+              </Pressable>
+            )
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
--- app/(tabs)/cargoAdd.tsx
+++ app/(tabs)/cargoAdd.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from "react-native";
 
-export default function Home() {
+export default function CargoAdd() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>CargoAdd Screen</Text>
     </View>
   );
 }
