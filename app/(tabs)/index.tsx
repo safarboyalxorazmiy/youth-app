d9e04c424f6dd09130be39ac3ecce3001326f85a
Safarboy ibn Anvar al-Xorazmiy <safarboyalkhwarizmi@gmail.com>
2025-03-14 17:16:37 +0500

    Home screen design fully compleated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,176 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, ScrollView } from "react-native";
+import ArrowRightIcon from "@/assets/images/navbar/ArrowRightIcon.svg";
+import TruckDeliverySpeedIcon from "@/assets/images/navbar/TruckDeliverySpeedIcon.svg";
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#EFEFEF" }}>
+      <ScrollView>
+        <View style={{paddingHorizontal: 30, paddingTop: 17, paddingBottom: 26, backgroundColor: "#FFF"}}>
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", height: 39, width: "100%"}}>
+            <View style={{alignItems: "center", justifyContent: "center", width: 86, height: 39, backgroundColor: "#2CA82A", borderRadius: 8}}>
+              <Text style={{fontSize: 16, color: "#FFF", fontWeight: 400}}>Yangi</Text>
+            </View>
+
+            <Text>20 daqiqa oldin</Text>
+          </View>
+
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginTop: 20}}>
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+
+            <ArrowRightIcon />
+
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+          </View>
+
+          <View style={{flexDirection: "row", alignItems: "center", marginTop: 20, columnGap: 10}}>
+            <TruckDeliverySpeedIcon />
+            <Text style={{fontSize: 20, fontWeight: 400}}>Transport turi: ISUZU</Text>
+          </View>
+
+          <Pressable style={{alignItems: "center", justifyContent: "center", marginTop: 13, height: 60, backgroundColor: "#000000", borderRadius: 11}}>
+            <Text style={{fontSize: 16, fontWeight: 700, color: "#FFF"}}>BATAFSIL</Text>
+          </Pressable>
+        </View>
+        
+        <View style={{marginTop: 14, paddingHorizontal: 30, paddingTop: 17, paddingBottom: 26, backgroundColor: "#FFF"}}>
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", height: 39, width: "100%"}}>
+            <View style={{alignItems: "center", justifyContent: "center", width: 86, height: 39, backgroundColor: "#2CA82A", borderRadius: 8}}>
+              <Text style={{fontSize: 16, color: "#FFF", fontWeight: 400}}>Yangi</Text>
+            </View>
+
+            <Text>20 daqiqa oldin</Text>
+          </View>
+
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginTop: 20}}>
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+
+            <ArrowRightIcon />
+
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+          </View>
+
+          <View style={{flexDirection: "row", alignItems: "center", marginTop: 20, columnGap: 10}}>
+            <TruckDeliverySpeedIcon />
+            <Text style={{fontSize: 20, fontWeight: 400}}>Transport turi: ISUZU</Text>
+          </View>
+
+          <Pressable style={{alignItems: "center", justifyContent: "center", marginTop: 13, height: 60, backgroundColor: "#000000", borderRadius: 11}}>
+            <Text style={{fontSize: 16, fontWeight: 700, color: "#FFF"}}>BATAFSIL</Text>
+          </Pressable>
+        </View>
+
+        <View style={{marginTop: 14, paddingHorizontal: 30, paddingTop: 17, paddingBottom: 26, backgroundColor: "#FFF"}}>
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", height: 39, width: "100%"}}>
+            <View style={{alignItems: "center", justifyContent: "center", width: 86, height: 39, backgroundColor: "#2CA82A", borderRadius: 8}}>
+              <Text style={{fontSize: 16, color: "#FFF", fontWeight: 400}}>Yangi</Text>
+            </View>
+
+            <Text>20 daqiqa oldin</Text>
+          </View>
+
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginTop: 20}}>
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+
+            <ArrowRightIcon />
+
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+          </View>
+
+          <View style={{flexDirection: "row", alignItems: "center", marginTop: 20, columnGap: 10}}>
+            <TruckDeliverySpeedIcon />
+            <Text style={{fontSize: 20, fontWeight: 400}}>Transport turi: ISUZU</Text>
+          </View>
+
+          <Pressable style={{alignItems: "center", justifyContent: "center", marginTop: 13, height: 60, backgroundColor: "#000000", borderRadius: 11}}>
+            <Text style={{fontSize: 16, fontWeight: 700, color: "#FFF"}}>BATAFSIL</Text>
+          </Pressable>
+        </View>
+
+        <View style={{marginTop: 14, paddingHorizontal: 30, paddingTop: 17, paddingBottom: 26, backgroundColor: "#FFF"}}>
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", height: 39, width: "100%"}}>
+            <View style={{alignItems: "center", justifyContent: "center", width: 86, height: 39, backgroundColor: "#2CA82A", borderRadius: 8}}>
+              <Text style={{fontSize: 16, color: "#FFF", fontWeight: 400}}>Yangi</Text>
+            </View>
+
+            <Text>20 daqiqa oldin</Text>
+          </View>
+
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginTop: 20}}>
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+
+            <ArrowRightIcon />
+
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+          </View>
+
+          <View style={{flexDirection: "row", alignItems: "center", marginTop: 20, columnGap: 10}}>
+            <TruckDeliverySpeedIcon />
+            <Text style={{fontSize: 20, fontWeight: 400}}>Transport turi: ISUZU</Text>
+          </View>
+
+          <Pressable style={{alignItems: "center", justifyContent: "center", marginTop: 13, height: 60, backgroundColor: "#000000", borderRadius: 11}}>
+            <Text style={{fontSize: 16, fontWeight: 700, color: "#FFF"}}>BATAFSIL</Text>
+          </Pressable>
+        </View>
+
+        <View style={{marginTop: 14, paddingHorizontal: 30, paddingTop: 17, paddingBottom: 26, backgroundColor: "#FFF"}}>
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", height: 39, width: "100%"}}>
+            <View style={{alignItems: "center", justifyContent: "center", width: 86, height: 39, backgroundColor: "#2CA82A", borderRadius: 8}}>
+              <Text style={{fontSize: 16, color: "#FFF", fontWeight: 400}}>Yangi</Text>
+            </View>
+
+            <Text>20 daqiqa oldin</Text>
+          </View>
+
+          <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginTop: 20}}>
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+
+            <ArrowRightIcon />
+
+            <View style={{width: 105, alignItems: "center"}}>
+              <Text style={{fontSize: 25, fontWeight: 700}}>Toshkent</Text>
+              <Text style={{fontSize: 18, fontWeight: 400}}>Bekobod</Text>
+            </View>
+          </View>
+
+          <View style={{flexDirection: "row", alignItems: "center", marginTop: 20, columnGap: 10}}>
+            <TruckDeliverySpeedIcon />
+            <Text style={{fontSize: 20, fontWeight: 400}}>Transport turi: ISUZU</Text>
+          </View>
+
+          <Pressable style={{alignItems: "center", justifyContent: "center", marginTop: 13, height: 60, backgroundColor: "#000000", borderRadius: 11}}>
+            <Text style={{fontSize: 16, fontWeight: 700, color: "#FFF"}}>BATAFSIL</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
     </View>
   );
 }
